4ef16f6601f4a6a46ffa8887b5407abc7c89a537
Galuh Danutirto <galuh.danutirto@opencloud.id>
2023-01-02 10:23:36 +0700

    [ADD] Add Card Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', data: {title: 'Dashboard'}, loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'dashboard', data: {title: 'Dashboard'}, loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'document-list', data: {title: 'Document List'}, loadChildren: () => import('./pages/document-list/document-list.module').then(m => m.DocumentListModule) },
 ];
