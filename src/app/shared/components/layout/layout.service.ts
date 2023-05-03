1446b317a0a4143b396869608cb0cc97f4537583
Galuh Danutirto <galuh.danutirto@opencloud.id>
2023-05-03 11:17:26 +0700

    [update] update api.gmf

--- src/app/shared/components/layout/layout.service.ts
+++ src/app/shared/components/layout/layout.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
@@ -11,10 +11,13 @@ export class LayoutService {
 
     constructor(private http: HttpClient) { }
 
+    headers = new HttpHeaders()
+        .set('x-api-key', '343C-ED0B-4137-B27E');
+
     getUserData(personal_number: any): Observable<any> {
         const params = new HttpParams()
             .set('personal_number', personal_number);
-        return this.http.get('https://api.gmf-aeroasia.co.id/th/soe/v1/employee/' + personal_number)
+        return this.http.get('https://api.gmf-aeroasia.co.id/th/soe/v1/employee/' + personal_number, { headers: this.headers })
             .pipe(
                 map((response) => {
                     return response;
